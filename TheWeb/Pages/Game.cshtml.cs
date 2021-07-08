789cde43fde3bc235f406df4a053d768d66fbe3f
Vera-Alejandro <alejandro.vera-gonzalez@trojans.dsu.edu>
2021-07-08 08:52:16 -0500

    added images to frontend calls

--- TheWeb/Pages/Game.cshtml.cs
+++ TheWeb/Pages/Game.cshtml.cs
@@ -1,19 +1,31 @@
 using Blackjack.GamePlay;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace TheWeb.Pages
 {
     public class GameModel : PageModel
     {
-        public string Username { get; set; } = "Guest";
-        public int DealerScore { get; set; } = 0;
-        public int PlayerScore { get; set; } = 0;
-        public int PlayerCash { get; set; } = 0;
-        public GameInstance CurrentGame { get; set; } = new GameInstance();
+        public GameInstance CurrentGame { get; private set; }
+
+        public string Username { get; } = "Guest";
+        public int DealerScore { get; } = 0;
+        public int PlayerScore { get; } = 0;
+        public float PlayerCash { get; } = 0f;
+        public bool GameStarted { get; private set; } = false;
 
 
         public void OnGet()
         {
+        }
+
+        public IActionResult StartGame()
+        {
+            CurrentGame ??= new GameInstance();
+
+            GameStarted = true;
+
+            return new OkResult();
         }
     }
 }
