e6064b284389bf5610a508ddbbd55b34c5b6a1c9
Vera-Alejandro <alejandro.vera-gonzalez@trojans.dsu.edu>
2021-07-09 10:52:42 -0500

    used vuex to pull player cash

--- blackjack/src/components/Game.vue
+++ blackjack/src/components/Game.vue
@@ -3,38 +3,57 @@
     <div class="p-5">
       <div class="flex flex-col">
         <div class="flex flex-row p-5 justify-between">
-            <div class="pr-5">
-                <img src="https://alejandropersonalstorage.blob.core.windows.net/public-images/BackJack-Title.png" alt="logo">
-            </div>
-            <div class="px-16">
-                <h1 class="text-white text-2xl"><u>Dealer</u></h1>
-                <h2 class="text-white">Score: {{ dealerScore }}</h2>
-            </div>
-            <div class="px-16">
-                <h1 class="text-white text-2xl"><u>Player</u></h1>
-                <h2 class="text-white">Score: {{ playerScore }}</h2>
-                <h2 class="text-white">Cash: ${{ playerCash }}</h2>
-            </div>
-        </div>
-        
-        <div class="grid grid-cols-2 gap-4 p-5">
-            <div v-for="cardImg in dealerCards" :key="cardImg.id" class="relative h-96 bg-white ">
-              <img class="absolute left-0 top-2" :src="cardImg.src" alt="card back">
-            </div>
-            <div class="h-96 bg-white relative">
-              <img class="absolute left-0 top-8" src="https://alejandropersonalstorage.blob.core.windows.net/public-images/Ace-of-Diamonds.png" alt="card back">
-              <img class="absolute left-10 top-8" src="https://alejandropersonalstorage.blob.core.windows.net/public-images/Ace-of-Diamonds.png" alt="card back">
-            </div>
+          <div class="pr-5">
+            <img
+              src="https://alejandropersonalstorage.blob.core.windows.net/public-images/BackJack-Title.png"
+              alt="logo"
+            />
+          </div>
+          <div class="px-16">
+            <h1 class="text-white text-2xl"><u>Dealer</u></h1>
+            <h2 class="text-white">Score: {{ dealerScore }}</h2>
+          </div>
+          <div class="px-16">
+            <h1 class="text-white text-2xl"><u>Player</u></h1>
+            <h2 class="text-white">Score: {{ playerScore }}</h2>
+            <h2 class="text-white">Cash: ${{ playerCash }}</h2>
+          </div>
         </div>
 
-        <div class="p-5 grid grid-cols-3 ">
-            <div class="col-span-2 inline-block">
-                <h3 class="text-white text-4xl pl-4">Bet</h3>
-            </div>
-            <div class="">
-                <button class="btn">Hit</button>
-                <button class="btn">Stay</button>
-            </div>
+        <div class="grid grid-cols-2 gap-4 p-5">
+          <div
+            v-for="cardImg in dealerCards"
+            :key="cardImg.id"
+            class="relative h-96 bg-white"
+          >
+            <img
+              class="absolute left-0 top-2"
+              :src="cardImg.src"
+              alt="card back"
+            />
+          </div>
+          <div class="h-96 bg-white relative">
+            <img
+              class="absolute left-0 top-8"
+              src="https://alejandropersonalstorage.blob.core.windows.net/public-images/Ace-of-Diamonds.png"
+              alt="card back"
+            />
+            <img
+              class="absolute left-10 top-8"
+              src="https://alejandropersonalstorage.blob.core.windows.net/public-images/Ace-of-Diamonds.png"
+              alt="card back"
+            />
+          </div>
+        </div>
+
+        <div class="p-5 grid grid-cols-3">
+          <div class="col-span-2 inline-block">
+            <h3 class="text-white text-4xl pl-4">Bet</h3>
+          </div>
+          <div class="">
+            <button class="btn">Hit</button>
+            <button class="btn">Stay</button>
+          </div>
         </div>
         <div class="flex flex-row p-5 justify-between">
           <div v-for="bet in possibleBets" :key="bet.Amount">
@@ -47,32 +66,40 @@
 </template>
 
 <script>
+// import gamePlayDataAccess from "../dataAccess/gamePlayDataAccess";
+
 export default {
-  name: 'Game',
+  name: "Game",
   data: () => ({
-      dealerScore: 0,
-      playerScore: 0,
-      possibleBets: [
-        {Amount: 9999, Text: "Bet All"},
-        {Amount: 1, Text: "$1"},
-        {Amount: 5, Text: "$5"},
-        {Amount: 10, Text: "$10"},
-        {Amount: 25, Text: "$25"},
-        {Amount: 50, Text: "$50"},
-        {Amount: 100, Text: "$100"},
-        {Amount: 250, Text: "$250"},
-        {Amount: 500, Text: "$500"},
-        {Amount: 1000, Text: "$1,000"},
-      ],
-      dealerCards: [
-        {id: 1, src: "https://alejandropersonalstorage.blob.core.windows.net/public-images/Ace-of-Diamonds.png", name: "Ace of Diamonds"},
-      ],
+    // gamePlayDAO: new gamePlayDataAccess(),
+    playerCash: 0,
+    dealerScore: 0,
+    playerScore: 0,
+    possibleBets: [
+      { Amount: 'All', Text: "Bet All" },
+      { Amount: 1, Text: "$1" },
+      { Amount: 5, Text: "$5" },
+      { Amount: 10, Text: "$10" },
+      { Amount: 25, Text: "$25" },
+      { Amount: 50, Text: "$50" },
+      { Amount: 100, Text: "$100" },
+      { Amount: 250, Text: "$250" },
+      { Amount: 500, Text: "$500" },
+      { Amount: 1000, Text: "$1,000" },
+    ],
+    dealerCards: [
+      {
+        id: 1,
+        src: "https://alejandropersonalstorage.blob.core.windows.net/public-images/Ace-of-Diamonds.png",
+        name: "Ace of Diamonds",
+      },
+    ],
   }),
-  computed: {
+  mounted: {
     playerCash() {
-      return this.$store.state.playerCash;
-    }
-  }
+      return 42;
+    },
+  },
 };
 </script>
 
--- blackjack/src/store/index.js
+++ blackjack/src/store/index.js
@@ -2,16 +2,11 @@ import { createStore } from "vuex";
 
 export default createStore({
     state: {
-        playerCash: 69,  
+        playerCash: 420,  
     },
     mutations: {
         updatePlayerCash(state, playerCash) {
             state.playerCash = playerCash;
         }
     },
-    actions: {
-        updatePlayerCash({ commit }, playerCash) {
-            commit('updatePlayerCash', playerCash);
-        }
-    }
 });
